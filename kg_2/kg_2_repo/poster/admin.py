7aebab1355f58ac2242ad16a6c43309df2a6616c
뭉가 <01rabbit10@gmail.com>
2025-08-07 15:41:13 +0900

    파일 정리: kg_2 삭제

--- kg_2/kg_2_repo/poster/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Poster
-# Register your models here.
-
-admin.site.register(Poster)
--- kg_2/kg_2_repo/poster/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'poster'
-
-urlpatterns = [
-    path('', views.poster_list, name='poster_list'),
-    path('<int:pk>/', views.poster_detail, name='poster_detail'),  # ✅ 상세 보기
-]
